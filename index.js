29d7b1998c17598dff2802f8e1395fbc24dcc893
kmunz <kmunz@hartenergy.com>
2016-08-05 09:17:55 -0500

    index.js created.

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('Loading CheckSslExpiry::');
+console.log('Version 0.1');
+
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    callback(null, 'Hello from Lambda');
+};
