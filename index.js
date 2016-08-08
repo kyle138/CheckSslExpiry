f330f273b808528cee090a963d88012c397d63d8
kmunz <kmunz@hartenergy.com>
2016-08-08 15:45:31 -0500

    removed moment and https, installed check-ssl-expiration

--- index.js
+++ index.js
@@ -2,32 +2,20 @@
 console.log('Loading CheckSslExpiry::');
 console.log('Version 0.1');
 
-var moment = require('moment');
-var https = require('https');
-
-var now = moment();
-
-var options = {
-  host: 'www.oilandgasinvestor.com',
-  port: 443,
-  method: 'GET'
-};
-
-var req = https.request(options, function(res) {
-  var cert=res.connection.getPeerCertificate();
-  var expireDate = moment(cert.valid_to, "MMM  D HH:mm:ss YYYY GMT");
-  var remaining = expireDate.diff(now, 'days');
-  console.log(cert.valid_to);
-  console.log("expireDate: "+expireDate);
-  console.log("now: "+now);
-  console.log("remaining: "+remaining);
-});
-
-req.end();
-
+var checkSsl = require('check-ssl-expiration');
 //Aug  8 15:51:44 2017 GMT
 
+var daysLeft = checkSsl('www.example.com', function(err, days) {
+  if(err) {
+    console.error(err);
+  } else {
+  console.log("days: "+days);
+  }
+});
+
+/*
 exports.handler = (event, context, callback) => {
     // TODO implement
     callback(null, 'Hello from Lambda');
 };
+*/
