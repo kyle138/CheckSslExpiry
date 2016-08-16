ba9844ad1f98cb3fd712f6b2a12ae8d84e01c48e
kmunz <kmunz@hartenergy.com>
2016-08-16 15:20:55 -0500

    added onScan()

--- index.js
+++ index.js
@@ -2,25 +2,42 @@
 console.log('Loading CheckSslExpiry::');
 console.log('Version 0.1');
 
+//// Local only, this will be replaced by IAM Role in Lambda
+var ddbOptions = require('./ddbOptions.json');
+var aws = require('aws-sdk');
+////
+
+
 var checkSsl = require('check-ssl-expiration');
+var ddb = new aws.DynamoDB(ddbOptions);
 
-var domains = [
-  'zeusintel.com',
-  'www.rextagstrategies.com',
-  'accounts.hartenergy.com',
-//  'report.hartenergynetwork.com',
-  'secure.hartenergynetwork.com',
-  'order.hartenergy.com',
-  'stratasadvisors.com',
-  'secure.oilandgasinvestor.com',
-  'hartenergy.com',
-  'store.hartenergy.com',
-  'www.oilandgasinvestor.com'
-];
+// var domains = [
+//   'zeusintel.com',
+//   'www.rextagstrategies.com',
+//   'accounts.hartenergy.com',
+// //  'report.hartenergynetwork.com',
+//   'secure.hartenergynetwork.com',
+//   'order.hartenergy.com',
+//   'stratasadvisors.com',
+//   'secure.oilandgasinvestor.com',
+//   'hartenergy.com',
+//   'store.hartenergy.com',
+//   'www.oilandgasinvestor.com'
+// ];
 
-domains.forEach(processDomains);
+function onScan(err, data, callback) {
+  if (err) {
+    console.error("Unable to scan the table. Error JSON: ",JSON.stringify(err, null, 2));
+  } else {
+    console.log("Scan succeeded. Items returned: "+data.Count); //DEBUG
+    data.Items.forEach(function(item) {
+      console.log("Item: "+JSON.stringify(item, null, 2));  //DEBUG
+    });
+  }
+}
 
-function processDomains(element, index, array) {
+
+function processDomain(element, index, array) {
   //console.log(element);
   checkSsl(element, 'days', function(err, remaining) {
     if(err) {
@@ -29,7 +46,6 @@ function processDomains(element, index, array) {
     console.log(element+": "+remaining);
     }
   });
-
 }
 
 
